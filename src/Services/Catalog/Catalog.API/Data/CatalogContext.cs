bae8576beabae3fe19679e68ee4046823af82be0
vbelovitsky <vbelovitsky@yandex.ru>
2022-05-11 00:50:01 +0300

    add Catalog service, create Data and Repositories

---
+++ src/Services/Catalog/Catalog.API/Data/CatalogContext.cs
@@ -0,0 +1,25 @@
+﻿using Catalog.API.Entities;
+using Catalog.API.Options;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Options;
+using MongoDB.Driver;
+
+namespace Catalog.API.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public IMongoCollection<Product> Products { get; }
+
+        public readonly DatabaseOptions _dbOptions;
+
+        public CatalogContext(IOptions<DatabaseOptions> dbOptions)
+        { 
+            _dbOptions = dbOptions.Value;
+            var client = new MongoClient(_dbOptions.ConnectionString);
+            var database = client.GetDatabase(_dbOptions.DatabaseName);
+            Products = database.GetCollection<Product>(_dbOptions.CollectionName);
+
+            CatalogContextSeed.SeedData(Products);
+        }
+    }
+}
---
+++ src/Services/Catalog/Catalog.API/Options/DatabaseOptions.cs
@@ -0,0 +1,10 @@
+﻿namespace Catalog.API.Options
+{
+    public class DatabaseOptions
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+
+        public string CollectionName { get; set; }
+    }
+}
