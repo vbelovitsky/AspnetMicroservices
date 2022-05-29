c5d601041a009a145dc44167b3f85b92870bb4ae
vbelovitsky <vbelovitsky@yandex.ru>
2022-05-29 21:02:46 +0300

    configure docker-compose for Catalog service

--- src/Services/Catalog/Catalog.API/Startup.cs
+++ src/Services/Catalog/Catalog.API/Startup.cs
@@ -48,12 +48,12 @@ namespace Catalog.API
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
+            //if (env.IsDevelopment() || env.)
+            //{
                 app.UseDeveloperExceptionPage();
                 app.UseSwagger();
                 app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Catalog.API v1"));
-            }
+            //}
 
             app.UseRouting();
 
